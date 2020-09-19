dcfc759ac9d6919b2f9f2b13d22fa4bde795c379
santiago dangond <santiagodangond@localhost.localdomain>
2020-09-19 00:09:19 -0500

    basedir added, initial config of the database

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
+basedir = os.path.abspath(os.path.dirname(__name__))
 
 
 app = Flask(__name__)
 app.debug = True
 app.env = 'development'
+app['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.sqlite')
+db  = SQLAlchemy(app)
+
 
 
 @app.route('/')
