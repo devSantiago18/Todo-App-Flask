45b81e5f734d6fed328ef47aba23e8fda71baff4
santiago dangond <santiagodangond@localhost.localdomain>
2020-09-19 11:14:13 -0500

    midels updated

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -8,10 +9,28 @@ basedir = os.path.abspath(os.path.dirname(__name__))
 app = Flask(__name__)
 app.debug = True
 app.env = 'development'
-app['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.sqlite')
+
 db  = SQLAlchemy(app)
+manager = Manager(app)
+
+# =========== Models =============
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(65), nullable=False)
+    email = db.Column(db.String(135), nullable=False)
+    password = db.Column(db.String(165),nullable=False)
+    tasks = db.relactionship('tasks', backref='user', lazy='dynamic')
 
 
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    text_task = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
+#================================
 
 @app.route('/')
 def index():
@@ -21,4 +40,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run() 
